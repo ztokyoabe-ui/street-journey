89e8872397f71b4f16649b716ee43c2555e0af1f
ztokyoabe-ui <ztokyoabe@gmail.com>
2026-06-09 17:04:04 +0900

    feat: use beta + maps3d library for vector/tilt support

--- pages/api/maps-init.js
+++ pages/api/maps-init.js
@@ -3,6 +3,6 @@ export default function handler(req, res) {
   if (!key) return res.status(500).json({ error: 'Maps JS API key not configured' });
   res.setHeader('Cache-Control', 'public, max-age=3600');
   res.status(200).json({
-    scriptUrl: `https://maps.googleapis.com/maps/api/js?key=${key}&v=weekly`
+    scriptUrl: `https://maps.googleapis.com/maps/api/js?key=${key}&v=beta&libraries=maps3d`
   });
 }
